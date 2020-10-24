b4b5e8ad407648b36623f9d51a5ef1d03e5e21c8
david <ahao@qq.com>
2020-10-24 19:10:11 +0800

    fff

---
+++ myWeb/src/main/java/com/hao/bean/User.java
@@ -0,0 +1,11 @@
+package com.hao.bean;
+
+/**
+ * @author hao   email：huanghao.0508@qq.com
+ * @ClassName：User
+ * @description ：TODO
+ * @create 2020-10-24-17:49
+ */
+public class User {
+    private String name;
+}
